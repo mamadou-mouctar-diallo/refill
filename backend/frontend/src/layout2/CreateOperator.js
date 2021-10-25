21587ebeb723b011d2e6d9ca081cac2febaa7b70
Mouctar <47394031+mamadou-mouctar-diallo@users.noreply.github.com>
2021-10-25 22:37:08 +0200

    update for app refill

---
+++ backend/frontend/src/layout2/CreateOperator.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class CreateOperator extends Component {
+render() { 
+  return (
+    <div>
+      
+    </div>
+  )}
+}
+
+export default CreateOperator
