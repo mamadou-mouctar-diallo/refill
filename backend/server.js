75ebe6934dc490c12b59b97c59c827cd378a67ec
Mouctar <47394031+mamadou-mouctar-diallo@users.noreply.github.com>
2021-10-27 17:43:40 +0200

    prepared for deploy

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const passport = require("passport");
 const users = require("./views/users");
 const operators = require("./views/operators");
 const mongoose = require("mongoose");
+const path = require("path");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -22,6 +23,15 @@ app.use(passport.initialize());
 require("./config/passport")(passport);
 app.use("/api/views/users/", users);
 app.use("/api/views/operators/", operators);
+
+// Server statuc assets if in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log("Server running on port", PORT));
